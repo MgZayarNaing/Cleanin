4b518e5dd9be3cd1821d4a73f6a9124c5e7885c2
MgZayarNaing <zayarnaing.st@gmail.com>
2025-08-07 21:09:29 +0630

    day2

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
+import uuid
 # Create your models here.
+
+class MainSliderModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    background_image = models.ImageField(upload_to='mainslider')
+    tagline = RichTextField()
+    title = RichTextField()
+    description = RichTextField()
+    thm_text = RichTextField()
+    thm_btn = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def HomePage(request):
-    return render(request, 'index.html')
+    mainslider = MainSliderModel.objects.all()
+    context = {
+        'mainslider':mainslider
+    }
+    return render(request, 'index.html',context)
